b68de20192ef4cfa73d283a7530f4a8082e92a82
Peter Mbugua <rockcerion42c@gmail.com>
2025-07-07 08:52:21 +0300

    institution and organisation structure models implementation

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth.admin import UserAdmin
-from Institution.models import Institution, Branch
+from Institution.models import Institution, InstBranch
+from Organisation.models import Organisation, OrgBranch
 
 import datetime
 
@@ -17,15 +18,18 @@ USER_TYPE = (
     ('organisational_staff', 'Organisational Staff'),
 )
 class CustomUser(AbstractUser):
+    username = None # removes user name
     first_name = models.CharField(max_length=200)
     middle_name = models.CharField(max_length=200, null=True, default='N/A')
     last_name = models.CharField(max_length=200)
     user_type = models.CharField(max_length=200, choices=USER_TYPE, default='student')
     email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = [email]
+
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    admission_number = models.CharField(max_length=100, unique=True)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    admission_number = models.CharField(max_length=100, unique=True, primary_key=True)
     year_of_admission = models.IntegerField(default=1)
     year_of_study = models.IntegerField(default=1)
     current_semester = models.IntegerField(default=1)
@@ -43,4 +47,26 @@ class StudentAdmin(models.Model):
 
 class Lecturer(UserAdmin):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class OrgStaff(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    staff_id = models.CharField(max_length=200, unique=True, primary_key=True)
+    staff_role = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    current_organisation = models.OneToOneField(Organisation, on_delete=models.DO_NOTHING, null=True)
+    current_org_branch = models.OneToOneField(OrgBranch, on_delete=models.DO_NOTHING, null=True)
+    previous_organisations = models.ManyToManyField(Organisation, null=True)
+    previous_org_branch = models.ManyToManyField(OrgBranch, null=True)
+    dob = models.DateField(auto_now=True)
+    nationality = models.CharField(max_length=300)
+    country_of_residence = models.CharField(max_length=300)
+    current_institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING, null=True)
+    current_inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    previous_institutions = models.ManyToManyField(Institution, null=True)
+    previous_inst_branch = models.ManyToManyField(InstBranch, null=True)
+    interests = models.CharField(max_length=5000) # TODO: remember to change this into a model reference of Interest
+    created_at = models.DateTimeField(auto_now=True)
+
+class OrgAdmin(models.Model):
+    user = models.OneToOneField(OrgStaff, on_delete=models.DO_NOTHING)
 
--- Institution/models.py
+++ Institution/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from Organisation.models import ORG_TYPES
 
 # Create your models here.
 class Institution(models.Model):
     name = models.CharField(max_length=1000)
+    inst_type = models.CharField(max_length=500, choices=ORG_TYPES, default='learning_inst')
     origin = models.CharField(max_length=500)
     abbreviation = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     postal_code = models.CharField(max_length=200)
     town = models.CharField(max_length=100)
     city = models.CharField(max_length=500)
+    academic_disc = models.CharField(max_length=5000)
     created_at = models.DateTimeField(auto_now=True)
 
-class Branch(models.Model):
+class InstBranch(models.Model):
     institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=200)
     branch_code = models.CharField(max_length=200, unique=True, primary_key=True)
@@ -21,4 +24,177 @@ class Branch(models.Model):
     postal_code = models.CharField(max_length=200)
     town = models.CharField(max_length=100)
     city = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+
+# University
+# ├── Office of the Vice Chancellor
+# ├── Faculties / Colleges / Schools
+# │   └── Departments
+# │       └── Programs / Courses
+# ├── Administrative Departments
+# │   ├── Registrar
+# │   ├── HR
+# │   ├── Finance
+# │   └── ICT
+# ├── Student Affairs
+# │   ├── Admissions
+# │   ├── Career Office
+# │   └── Counseling
+# └── Support Services
+#     ├── Security
+#     ├── Transport
+#     └── Health Services
+class VCOffice(models.Model):
+    institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
+    inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    office_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Faculty(models.Model):
+    institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
+    inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    faculty_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class InstDepartment(models.Model):
+    faculty = models.OneToOneField(Faculty, on_delete=models.DO_NOTHING)
+    dep_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Programme(models.Model):
+    faculty = models.OneToOneField(InstDepartment, on_delete=models.DO_NOTHING)
+    programme_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+# ├── Administrative Departments
+# │   ├── Registrar
+# │   ├── HR
+# │   ├── Finance
+# │   └── ICT
+# │   └── Legal
+
+class AdminDep(models.Model):
+    institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
+    inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    admin_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class RegistrarOffice(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    registrar_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class HR(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    hr_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class ICT(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    ict_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Finance(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    finance_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Marketing(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    marketing_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Legal(models.Model):
+    admin_dep = models.OneToOneField(AdminDep, on_delete=models.DO_NOTHING)
+    legal_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+# ├── Student Affairs
+# │   ├── Admissions
+# │   ├── Career Office
+# │   └── Counseling
+
+class StudentAffairs(models.Model):
+    institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
+    inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    stud_affairs_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Admissions(models.Model):
+    stud_affairs = models.OneToOneField(StudentAffairs, on_delete=models.DO_NOTHING)
+    admissions_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class CareerOffice(models.Model):
+    stud_affairs = models.OneToOneField(StudentAffairs, on_delete=models.DO_NOTHING)
+    career_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Couselling(models.Model):
+    stud_affairs = models.OneToOneField(StudentAffairs, on_delete=models.DO_NOTHING)
+    counselling_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+# └── Support Services
+#     ├── Security
+#     ├── Transport
+#     └── Health Services
+
+class SupportServices(models.Model):
+    institution = models.OneToOneField(Institution, on_delete=models.DO_NOTHING)
+    inst_branch = models.OneToOneField(InstBranch, on_delete=models.DO_NOTHING, null=True)
+    support_services_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Security(models.Model):
+    stud_affairs = models.OneToOneField(SupportServices, on_delete=models.DO_NOTHING)
+    security_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class Transport(models.Model):
+    stud_affairs = models.OneToOneField(SupportServices, on_delete=models.DO_NOTHING)
+    transport_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now=True)
+
+class HealthServices(models.Model):
+    stud_affairs = models.OneToOneField(SupportServices, on_delete=models.DO_NOTHING)
+    health_services_code = models.CharField(max_length=500, unique=True, primary_key=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField(max_length=5000)
     created_at = models.DateTimeField(auto_now=True)
