bfdd1b1c03fbcbe50fe1f62fc3d956bfbfca1313
Cerion73 <rockcerion42c@gmail.com>
2026-05-26 13:05:01 +0300

    Profile picture upload issues handling.

--- comrade/asgi.py
+++ comrade/asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comrade.settings')
 
 django_asgi_app = get_asgi_application()
@@ -23,12 +21,10 @@ from comrade.ws_auth import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        JWTAuthMiddleware(
-            URLRouter(
-                Rooms.routing.websocket_urlpatterns + 
-                Notifications.routing.websocket_urlpatterns
-            )
+    "websocket": JWTAuthMiddleware(
+        URLRouter(
+            Rooms.routing.websocket_urlpatterns +
+            Notifications.routing.websocket_urlpatterns
         )
     ),
 })
